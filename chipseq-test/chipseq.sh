ea29b65a394e2aea9055a29f95a3917af4c4dc11
Vincent Hus <jvf.hus@gmail.com>
2024-06-18 15:48:26 +0200

    adding chipseq tools

---
+++ chipseq-test/chipseq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# FASTQC - QC layer 1
+cd ./workflows/chipseq
+
+fastqc s1_1.fq s1_2.fq -o .
+
+# Creating a human genome index using STAR
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir ./human --genomeSAindexNbases 10 --genomeFastaFiles human.fa --sjdbGTFfile hg19.refGene.gtf --sjdbOverhang 99
+
+# Align RNA-Seq reads to the genome using STAR
+STAR --runThreadN 4 --genomeDir ./human --readFilesIn s1_1.fq s1_2.fq --outFileNamePrefix P1s1 --outSAMtype BAM SortedByCoordinate
+
+# Sort and Index the output bam file
+samtools sort *.bam -@ 4 -o P1s1.sorted.bam
+samtools index P1s1.sorted.bam
+
+# Call Peaks for BAM file
+macs3 callpeak -t P1s1.sorted.bam -f BAMPE -p 0.05 --outdir ./Peaks
