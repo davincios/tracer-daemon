f073c7f4f77760848877ae3cde61fa301c0f13db
actions-user <actions@github.com>
2024-07-19 12:33:10 +0000

    Update version to v0.0.77-dev

--- install-tracer.sh
+++ install-tracer.sh
@@ -6,7 +6,7 @@
 #-------------------------------------------------------------------------------
 SCRIPT_VERSION="v0.0.1"
 TRACER_VERSION="v0.0.75"
-TRACER_VERSION_DEVELOP="v0.0.76-dev"
+TRACER_VERSION_DEVELOP="v0.0.77-dev"
 
 TRACER_HOME="$HOME/.tracerbio"
 LOGFILE_NAME="tracer-installer.log"
