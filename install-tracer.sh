f8d9a1d15285754e6445e1a699259c72eb739522
actions-user <actions@github.com>
2024-08-08 16:36:45 +0000

    Update version to v0.0.105-dev

--- install-tracer.sh
+++ install-tracer.sh
@@ -6,7 +6,7 @@
 #-------------------------------------------------------------------------------
 SCRIPT_VERSION="v0.0.1"
 TRACER_VERSION="v0.0.104"
-TRACER_VERSION_DEVELOP="v0.0.102-dev"
+TRACER_VERSION_DEVELOP="v0.0.105-dev"
 
 TRACER_HOME="$HOME/.tracerbio"
 LOGFILE_NAME="tracer-installer.log"
